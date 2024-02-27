fd0c65b86eaf20fea2963997801c0500ce484bc3
Felipe Vasconcelos <felipesvascon@gmail.com>
2024-02-27 20:40:12 -0300

    refact: improve configs

--- src/database/orm-cli.config.ts
+++ src/database/orm-cli.config.ts
@@ -1,8 +1,21 @@
+import 'dotenv/config';
 import { CreateCoursesTable1708966105519 } from 'src/migrations/1708966105519-CreateCoursesTable';
-import { dataSourceOptions } from './database.module';
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { CreateTagsTable1708967084707 } from 'src/migrations/1708967084707-CreateTagsTable';
 import { CreateCoursesTagsTable1708967651399 } from 'src/migrations/1708967651399-CreateCoursesTagsTable';
+import { Tag } from 'src/courses/entities/tags.entity';
+import { Course } from 'src/courses/entities/courses.entity';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [Course, Tag],
+  synchronize: false
+}
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
